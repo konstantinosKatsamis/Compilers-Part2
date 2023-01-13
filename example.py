4271fa8ef450da492b0c023103d1dc1dcd0d516d
konstantinosKatsamis <75335809+konstantinosKatsamis@users.noreply.github.com>
2023-01-14 01:13:45 +0200

    Update example.py

--- example.py
+++ example.py
@@ -11,3 +11,15 @@ k=0
 num = 1 + type(k)
 
 print type(eneshei) + 4.5
+
+def add1(x,y,z):
+    print 1
+
+def add1(x,y,z=1):
+    print 1
+
+def add2(x,y):
+    print 1
+
+def add2(x,y,z=1):
+    print 1
