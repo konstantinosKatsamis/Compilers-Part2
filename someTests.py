e0395fc0778d31f609ef7c79c4c436b75e9893c3
konstantinosKatsamis <75335809+konstantinosKatsamis@users.noreply.github.com>
2023-01-15 02:42:36 +0200

    Create someTests.py

---
+++ someTests.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+# test1:
+def koita():
+    print "1"
+
+koita()
+
+def fata():
+    return "string"
+
+
+kapoiosiso = fata() + 1
+
+
+# test2
+
+def add2(x, y):
+    return "1 + 1"
+
+kaaaaaaaaaaaaaaaa = "hello world"
+print add2(1, k)
+
+def add(x,y):
+    return "hello world"
+print add(2,1)+2
